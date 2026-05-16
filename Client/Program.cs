eeb22c3466a36e0d3466afb55dbf84aad68c02c9
Maximilian Stoll <maxstoll94@gmail.com>
2026-05-16 23:58:27 +0200

    Hel

---
+++ Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using Temporalio.Client;
+using Workflow;
+
+// Create a client to localhost on "default" namespace
+var client = await TemporalClient.ConnectAsync(new("localhost:7233"));
+
+// Run workflow
+var result = await client.ExecuteWorkflowAsync(
+    (SayHelloWorkflow wf) => wf.RunAsync("Temporal"),
+    new(id: $"my-workflow-id-{Guid.NewGuid()}", taskQueue: "my-task-queue")
+);
+
+Console.WriteLine("Workflow result: {0}", result);
---
+++ Workflow/MyActivities.cs
@@ -0,0 +1,9 @@
+﻿using Temporalio.Activities;
+
+namespace Workflow;
+
+public class MyActivities
+{
+    [Activity]
+    public string SayHello(string name) => $"Hello {name}";
+}
